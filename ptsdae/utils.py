17fe3fa6555241674da55260acba63a4f7532129
Chongming Gao <chongming.gao@gmail.com>
2019-05-07 16:55:33 +0800

    fix parameters disordered problem
    1. Fix the parameters displacement.
    2. A slightly better style is given.

--- ptsdae/utils.py
+++ ptsdae/utils.py
@@ -47,4 +47,13 @@ def cluster_accuracy(y_true, y_predicted, cluster_number: Optional[int] = None):
         count_matrix[y_predicted[i], y_true[i]] += 1
     reassignment = np.dstack(linear_sum_assignment(count_matrix.max() - count_matrix))[0]
     accuracy = count_matrix[reassignment[:, 0], reassignment[:, 1]].sum() / y_predicted.size
-    return {item[1]: item[0] for item in reassignment}, accuracy
+    return {item[0]: item[1] for item in reassignment}, accuracy
+
+    """
+    # A better style for line 48~50 is as below: 
+    rowInd, colInd = linear_sum_assignment(count_matrix.max() - count_matrix)
+    reassignment = dict(zip(rowInd,colInd))
+    accuracy = count_matrix[rowInd, colInd].sum() / y_predicted.size
+    return reassignment, accuracy
+    """
+
