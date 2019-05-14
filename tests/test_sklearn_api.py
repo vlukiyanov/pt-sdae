ebeec783e0eece3c6406757c6c5e9315cbe6d1e2
Vladimir Lukiyanov <vladimir.lukiyanov@me.com>
2019-05-14 23:42:19 +0100

    Add scikit-learn style API (#18)
    * fix PEP8
    
    * add a test of reassignment
    
    * add another test of the cluster reassignment
    
    * add basic sklearn-api
    
    * add faker requirement

---
+++ tests/test_sklearn_api.py
@@ -0,0 +1,46 @@
+from unittest import TestCase
+
+from faker import Faker
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import make_pipeline, Pipeline
+
+from ptsdae.sklearn_api import SDAETransformer
+
+
+class TestSDAETransformer(TestCase):
+    def test_basic(self):
+        fake = Faker()
+        fake.seed(0)
+        pipeline = make_pipeline(
+            CountVectorizer(
+                stop_words='english',
+                max_features=25,
+                max_df=0.9
+            ),
+            SDAETransformer(
+                dimensions=[25, 10, 2],
+                pretrain_epochs=1,
+                finetune_epochs=1
+            )
+        )
+        pipeline.fit([fake.text() for _ in range(100)])
+        result = pipeline.transform([fake.text() for _ in range(20)])
+        self.assertEqual(result.shape, (20, 2))
+
+    def test_score(self):
+        fake = Faker()
+        fake.seed(0)
+        pipeline = Pipeline(steps=[
+            ('vectorizer', CountVectorizer(
+                stop_words='english',
+                max_features=25,
+                max_df=0.9
+            )),
+            ('ae', SDAETransformer(dimensions=[25, 10, 2], finetune_epochs=1))
+        ])
+        param_grid = {
+            'ae__pretrain_epochs': [1, 2, 3],
+        }
+        search = GridSearchCV(pipeline, param_grid, iid=False, cv=2, return_train_score=False)
+        search.fit([fake.text() for _ in range(10)])
